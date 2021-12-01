0f67b847214d83679eaa5b53c5d6d6588632a553
Barnaby Gray <barnaby@pickle.me.uk>
2021-12-01 06:18:06 +0000

    Day 1

---
+++ day01.nim
@@ -0,0 +1,15 @@
+import common
+
+var numbers = parseIntList(readFile "input01.txt")
+
+proc increases(ns: seq[int], stride: int): int =
+    for i in 0..<ns.len-stride:
+        if ns[i+stride] > ns[i]:
+            inc result
+
+proc solve: Answer =
+    result.part1 = increases(numbers, 1)
+    # Each sum of threes has the 1st and 2nd in common with the next threes, so only necessary to compare 0th and 3rd
+    result.part2 = increases(numbers, 3)
+
+echo solve()
