66502f317032f5658d13cbefffda1d30aa636283
Barnaby Gray <barnaby@pickle.me.uk>
2021-12-03 19:52:51 +0000

    Day 3 simplifications

--- src/day03.nim
+++ src/day03.nim
@@ -1,30 +1,22 @@
 import common, sequtils
 
-proc fromBin(s: string): int =
-    for c in s:
-        result = result*2 + (if c == '1': 1 else: 0)
+proc fromBin(s: string): int = 
+    for col in 0..s.high:
+        result = (result shl 1) + int(s[col] == '1')
 
-proc countColumn(lines: seq[string], col: int): int =
-    for line in lines:
-        if line[col] == '1':
-            inc result
+proc countColumn(lines: seq[string], col: int): int = countIt(lines, it[col] == '1')
 
 proc filterDown(lines: seq[string], col: int, pick: string): string =
-    var common = (if countColumn(lines, col) >= (lines.len+1) div 2: pick[0] else: pick[1])
-    var selected: seq[string]
-    for p in lines:
-        if p[col] == common: selected.add(p)
-    if selected.len == 1:
-        return selected[0]
-    return filterDown(selected, col+1, pick)
-
+    let common = (if countColumn(lines, col) >= (lines.len+1) div 2: pick[0] else: pick[1])
+    let selected = lines.filterIt(it[col] == common)
+    return if selected.len > 1: filterDown(selected, col+1, pick) else: selected[0]
 
 proc solve*(input: string): Answer =
     let lines = toSeq(lines input)
     let width = lines[0].len
-    var gamma: int
+    var gamma = 0
     for col in 0..<width:
-        gamma = (gamma shl 1)
+        gamma = gamma shl 1
         if countColumn(lines, col) > lines.len div 2:
             inc gamma
     let epsilon = gamma xor ((1 shl width)-1)
