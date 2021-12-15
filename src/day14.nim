5600008435bf4676832fce424282ccece900b56c
Barnaby Gray <barnaby@pickle.me.uk>
2021-12-15 19:55:35 +0000

    Day 14

--- src/day14.nim
+++ src/day14.nim
@@ -8,21 +8,30 @@ proc solve*(input: string): Answer =
         if line == "": continue
         rules[line[0..1]] = line[6]
 
-    for i in 1..40:
-        var next: string
-        for j in 0..current.len-2:
-            next &= current[j]
-            next &= rules[current[j..j+1]]
-        next &= current[^1]
-        current = next
+    var bag: CountTable[string]
+    for i in 0..current.len-2:
+        bag.inc current[i..i+1]
 
-        # echo i, " ", current
+    for i in 1..40:
+        var nextBag: CountTable[string]
+        for pair, count in bag:
+            let c = rules[pair]
+            nextBag.inc(pair[0] & c, count)
+            nextBag.inc(c & pair[1], count)
+        bag = nextBag
+
         var counts: CountTable[char]
-        for c in current:
-            counts.inc(c)
-        echo i, " ", current.len, " ", counts
+        for pair, count in bag:
+            counts.inc(pair[0], count)
+            counts.inc(pair[1], count)
+        # add start and end
+        counts.inc(current[0])
+        counts.inc(current[^1])
+        # everything is double counted now, so half
+        for c, count in counts:
+            counts[c] = count div 2
 
         if i == 10:
             result.part1 = counts.largest[1] - counts.smallest[1]
-    
-    result.part2 = 0
+        if i == 40:
+            result.part2 = counts.largest[1] - counts.smallest[1]
--- tests/test14.nim
+++ tests/test14.nim
@@ -7,4 +7,4 @@ suite "day 14":
     check answer.part1 == 1588
   test "part 2":
     let answer = day14.solve("tests/test14.txt")
-    check answer.part2 == 0
+    check answer.part2 == 2188189693529
